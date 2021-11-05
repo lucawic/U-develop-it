155350033f3dffb1ea265cc92a4d7a41d04a50bf
lucawic <lucaswick@email.arizona.edu>
2021-11-04 20:31:33 -0700

    fixed GET Route

--- server.js
+++ server.js
@@ -1,6 +1,37 @@
 const express = require('express');
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 const app = express();
+
+const mysql = require('mysql2');
+
+// Get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
+  
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+  
+//conection to DB
+const db = mysql.createConnection(
+    {
+        host:'localhost',
+        //Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password:'password',
+        database:'election'
+    },
+    console.log('Connected to the election database')
+);
 
 //middleware for express.js
 app.use(express.urlencoded({ extended: false }));
